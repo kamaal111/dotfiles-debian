760ff4d57b30cb4cc8c5a6890c4b9cac2ab0ab96
Kamaal Farah <kamaal.f1@gmail.com>
2024-09-17 20:38:53 +0200

    Installing caddy

--- scripts/install-dependencies.zsh
+++ scripts/install-dependencies.zsh
@@ -30,3 +30,16 @@ then
 else
     sudo apt install -y gh
 fi
+
+if ! command -v caddy &> /dev/null
+then
+    sudo apt remove apache2
+    sudo apt update
+    sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+    sudo apt update
+    sudo mkdir -p /etc/caddy
+fi
+
+sudo apt install -y caddy
